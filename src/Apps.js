4b1ee14162a4ff75d388da121680266faa6a5635
seongto <smalllife@naver.com>
2019-06-05 13:26:40 +0900

    기본 셋팅 및 Home 구성

---
+++ src/Apps.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from './Routes';
+
+
+class Apps extends React.Component {
+  render(){
+    return (
+      <div className="flex-container">
+        <Routes />
+      </div>
+    )
+  }
+}
+
+export default Apps;
